3922619704c3247208c4af4d761e837b6b5e29f2
KayDrew <kdtshehla@gmail.com>
2023-07-28 07:05:20 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+import express  from 'express';
+import bodyParser from 'body-parser';
+import { engine } from 'express-handlebars';
+import SettingsBill from './settings-bill.js';
+const app = express();
+
+
+
+app.use(express.static('public'));
+app.engine('handlebars', engine());
+
+app.set('view engine', 'handlebars');
+
+app.set('views', './views');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+const settingsBill=SettingsBill();
+
+app.get('/', function (req, res) {
+
+    res.render('index',{settings: settingsBill.getSettings()});
+
+});
+
+app.post('/settings', function (req, res) {
+    
+settingsBill.setSettings({
+    
+    callCost:req.body.callCost,
+    smsCost: req.body.smsCost,
+    warningLevel: req.body.warningLevel,
+    criticalLevel: req.body.criticalLevel
+}
+
+);
+    console.log(settingsBill.getSettings());
+    res.redirect('/');
+
+});
+
+app.post('/action', function (req, res) {
+
+    
+
+});
+
+app.get('/actions', function (req, res) {
+
+    
+
+});
+
+app.get('/actions/:type', function (req, res) {
+
+    
+
+});
+
+
+
+
+let PORT = process.env.PORT || 3007;
+
+
+
+app.listen(PORT, function(){
+
+  console.log('App starting on port', PORT);
+
+});
